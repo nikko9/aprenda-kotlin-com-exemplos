7b91a3d3f7d48e6f3cfa00c067df6f6a67319927
Claudenir Rodrigues Araujo <62363683+nikko9@users.noreply.github.com>
2023-01-13 01:15:19 -0300

    verificacoes de igualdade

--- 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
+++ 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
@@ -4,5 +4,9 @@
  * @see [Equality Checks](https://play.kotlinlang.org/byExample/02_control_flow/04_Equality%20Checks)
  */
 fun main() {
-    
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)     //retorna true pois testa se os valores sao iguais ignorando a ordem dos elementos.
+    println(authors === writers)    //retorna falso por fazer uma comparacao referencial.
  }
