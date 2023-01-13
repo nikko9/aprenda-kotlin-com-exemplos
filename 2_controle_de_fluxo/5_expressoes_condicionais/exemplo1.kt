6ecb8705305bd5fe1d5d72be55f544320e49821a
Claudenir Rodrigues Araujo <62363683+nikko9@users.noreply.github.com>
2023-01-13 01:19:22 -0300

    expressoes condicionais

--- 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
+++ 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
@@ -4,5 +4,7 @@
  * @see [Conditional Expression](https://play.kotlinlang.org/byExample/02_control_flow/05_Conditional%20expression)
  */
 fun main() {
-    
+    fun max(a: Int, b: Int) = if (a > b) a else b   //if é uma expressao aqui e retorna um valor.
+
+    println(max(99, -42))
  }
