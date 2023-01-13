d87e1561051d12433917b798c9a6228392eb8c53
Claudenir Rodrigues Araujo <62363683+nikko9@users.noreply.github.com>
2023-01-13 01:05:45 -0300

    intervalos com condicionais

--- 2_controle_de_fluxo/3_ranges/exemplo3.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo3.kt
@@ -4,5 +4,13 @@
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
 fun main() {
-    
+    val x = 2
+    if (x in 1..5) {
+        print("x está entre 1 e 5")
+    }
+    println()
+
+    if (x !in 6..10) {
+        print("x nao esta entre 6 e 10")
+    }
  }
