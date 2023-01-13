86cd9221047488e0e43eaf5dbc26bd30158b8d9b
Claudenir Rodrigues Araujo <62363683+nikko9@users.noreply.github.com>
2023-01-13 00:52:16 -0300

    incluindo formas de utilizar range

--- 2_controle_de_fluxo/3_ranges/exemplo1.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo1.kt
@@ -4,5 +4,24 @@
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
 fun main() {
-    
+    for(i in 0..3) {  //incrementa de 0 até 3 incluindo o próprio 3
+        print(i)
+    }
+    print(" ")
+
+    for(i in 0 until 3) {  //incrementa de 0 até 3 excluindo 3.
+        print(i)
+    }
+    print(" ")
+
+    for(i in 2..8 step 2) {  //incrementa de forma personalizada, saltando em valores especificados.
+        print(i)
+    }
+    print(" ")
+
+    for(i in 3 downTo 0) {
+        print(i)
+    }
+    print(i)
  }
+ 
