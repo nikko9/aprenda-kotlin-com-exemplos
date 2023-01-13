cfe03e7ccd505472434e128d6f242d161985f343
Claudenir Rodrigues Araujo <62363683+nikko9@users.noreply.github.com>
2023-01-13 00:58:38 -0300

    ranges de chars

--- 2_controle_de_fluxo/3_ranges/exemplo2.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo2.kt
@@ -4,5 +4,13 @@
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
 fun main() {
-    
+    for (c in 'a'..'d') {   //incrementa caracteres em ordem alfabetica.
+        print(c)
+    }
+    print(" ")
+
+    for (c in 'z' downTo 's' step 2) {  //incremento em ordem inversa a alfabetica e saltando chars.
+        print(c)
+    }
+    print(" ")
  }
